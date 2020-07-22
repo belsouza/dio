07286c0fb989079eee44b673c632d9d3d48b8e92
belsanpesou <belsanpesou@yahoo.com.br>
2020-07-22 16:55:02 -0300

    Entendendo o Spring Web MVC

---
+++ spring-web-mvc/src/main/java/br/com/globallabs/springwebmvc/repository/JediRepository.java
@@ -0,0 +1,29 @@
+package br.com.globallabs.springwebmvc.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import br.com.globallabs.springwebmvc.model.Jedi;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JediRepository {
+
+    private List<Jedi> jedi;
+
+    public JediRepository() {
+        jedi = new ArrayList<>();
+        jedi.add(new Jedi("Luke", "Skywalker"));
+        jedi.add(new Jedi("Obi-Wan", "Kenobi"));
+        jedi.add(new Jedi("Qui-Gon", "Jinn"));
+    }
+
+    public List<Jedi> getAll() {
+        return jedi;
+    }
+
+    public void add(final Jedi jedi) {
+        this.jedi.add(jedi);
+    }
+}
